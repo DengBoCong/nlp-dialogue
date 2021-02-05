ef10e934922e4b2b46524cae66b0b1ebdeddbe7e
DBC <1210212670@qq.com>
2021-02-05 16:37:16 +0800

    完成pytorch版seq2seq构建

--- dialogue/pytorch/layers.py
+++ dialogue/pytorch/layers.py
@@ -1,3 +1,23 @@
+# Copyright 2021 DengBoCong. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""公用层组件
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -7,14 +27,15 @@ from typing import Tuple
 class BahdanauAttention(nn.Module):
     """ bahdanau attention实现
 
-    :param units: 隐层单元数
+    :param enc_units: encoder单元大小
+    :param dec_units: decoder单元大小
     """
 
-    def __init__(self, units: int) -> None:
+    def __init__(self, enc_units: int, dec_units: int) -> None:
         super(BahdanauAttention, self).__init__()
-        self.W1 = nn.Linear(in_features=2 * units, out_features=units)
-        self.W2 = nn.Linear(in_features=units, out_features=units)
-        self.V = nn.Linear(in_features=units, out_features=1)
+        self.W1 = nn.Linear(in_features=2 * enc_units, out_features=dec_units)
+        self.W2 = nn.Linear(in_features=2 * enc_units, out_features=dec_units)
+        self.V = nn.Linear(in_features=dec_units, out_features=1)
 
     def forward(self, query: torch.Tensor, values: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         """
@@ -22,7 +43,7 @@ class BahdanauAttention(nn.Module):
         :param values: encoder输出状态
         """
         values = values.permute(1, 0, 2)
-        hidden_with_time_axis = torch.unsqueeze(input=query, dim=1)
+        hidden_with_time_axis = torch.unsqueeze(input=query, dim=1).repeat(1, values.shape[1])
         score = self.V(torch.tanh(self.W1(values) + self.W2(hidden_with_time_axis)))
 
         attention_weights = F.softmax(input=score, dim=1)
--- dialogue/pytorch/seq2seq/model.py
+++ dialogue/pytorch/seq2seq/model.py
@@ -1,23 +1,41 @@
+# Copyright 2021 DengBoCong. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""seq2seq的Pytorch实现核心core
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from typing import Tuple
 
 
 class Encoder(nn.Module):
     """ seq2seq的encoder """
 
-    def __init__(self, vocab_size: int, embedding_dim: int, enc_units: int, cell_type: str,
-                 dec_units: int, num_layers: int, dropout: float, if_bidirectional: bool = True) -> None:
+    def __init__(self, vocab_size: int, embedding_dim: int, enc_units: int, num_layers: int,
+                 dropout: float, cell_type: str = "lstm", if_bidirectional: bool = True) -> None:
         """
         :param vocab_size: 词汇量大小
         :param embedding_dim: 词嵌入维度
         :param enc_units: encoder单元大小
-        :param cell_type: cell类型，lstm/gru， 默认lstm
-        :param dec_units: decoder单元大小
         :param num_layers: encoder中内部RNN层数
         :param dropout: 采样率
         :param if_bidirectional: 是否双向
+        :param cell_type: cell类型，lstm/gru， 默认lstm
         :return: Seq2Seq的Encoder
         """
         super(Encoder, self).__init__()
@@ -31,53 +49,65 @@ class Encoder(nn.Module):
             self.rnn = nn.GRU(input_size=embedding_dim, hidden_size=enc_units,
                               num_layers=num_layers, bidirectional=if_bidirectional)
 
-        self.fc = nn.Linear(in_features=enc_units * 2, out_features=dec_units)
         self.dropout = nn.Dropout(p=dropout)
 
-    def forward(self, inputs: torch.Tensor) -> Tuple[torch.Tensor]:
+    def forward(self, inputs: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        :param inputs: encoder的输入
+        """
         inputs = self.embedding(inputs)
         dropout = self.dropout(inputs)
         outputs, state = self.rnn(dropout)
         # 这里使用了双向GRU，所以这里将两个方向的特征层合并起来，维度将会是units * 2
-        state = torch.tanh(self.fc(torch.cat((state[-2, :, :], state[-1, :, :]), dim=1)))
+        state = torch.cat((state[-2, :, :], state[-1, :, :]), dim=1)
         return outputs, state
 
 
-
-
-
 class Decoder(nn.Module):
-    """
-    seq2seq的decoder，将初始化的inputs、隐藏层和encoder的输出作为
-    输入，encoder的输入用来和隐藏层进行attention，得到的上下文
-    向量和x进行整合然后丢到gru里去，最后Linear输出一下
+    """ seq2seq的decoder
+
+    :param vocab_size: 词汇量大小
+    :param embedding_dim: 词嵌入维度
+    :param enc_units: encoder单元大小
+    :param dec_units: decoder单元大小
+    :param num_layers: encoder中内部RNN层数
+    :param dropout: 采样率
+    :param attention: 用于计算attention
+    :param cell_type: cell类型，lstm/gru， 默认lstm
+    :param if_bidirectional: 是否双向
+    :return: Seq2Seq的Encoder
     """
 
-    def __init__(self, vocab_size: int, embedding_dim: int, enc_units: int,
-                 dec_units: int, dropout: float, attention: nn.Module):
+    def __init__(self, vocab_size: int, embedding_dim: int, enc_units: int, dec_units: int, num_layers: int,
+                 dropout: float, attention: nn.Module, cell_type: str = "lstm", if_bidirectional: bool = True) -> None:
         super(Decoder, self).__init__()
         self.vocab_size = vocab_size
         self.attention = attention
-        self.embedding = nn.Embedding(
-            num_embeddings=vocab_size, embedding_dim=embedding_dim)
-        self.gru = nn.GRU(input_size=(enc_units * 2) +
-                                     embedding_dim, hidden_size=dec_units)
-        self.fc = nn.Linear(in_features=(enc_units * 3) +
-                                        embedding_dim, out_features=vocab_size)
+        self.embedding = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embedding_dim)
+        if cell_type == "lstm":
+            self.rnn = nn.LSTM(input_size=enc_units * 2 + embedding_dim, hidden_size=dec_units,
+                               num_layers=num_layers, bidirectional=if_bidirectional)
+        elif cell_type == "gru":
+            self.rnn = nn.GRU(input_size=enc_units * 2 + embedding_dim, hidden_size=dec_units,
+                              num_layers=num_layers, bidirectional=if_bidirectional)
+        self.fc = nn.Linear(in_features=2 * enc_units + 2 * dec_units + embedding_dim, out_features=vocab_size)
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, inputs: torch.Tensor, hidden: torch.Tensor, enc_output: torch.Tensor) -> Tuple[torch.Tensor]:
-        inputs = inputs.unsqueeze(0)
+    def forward(self, inputs: torch.Tensor, hidden: torch.Tensor,
+                enc_output: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        :param inputs: decoder的输入
+        :param hidden: encoder的hidden
+        :param enc_output: encoder的输出
+        """
+        embedding = self.embedding(inputs)
+        embedding = self.dropout(embedding)
         context_vector, attention_weights = self.attention(hidden, enc_output)
 
-        embedding = self.dropout(self.embedding(inputs))
-        gru_inputs = torch.cat(
-            (embedding, torch.unsqueeze(context_vector, dim=0)), dim=-1)
-        output, dec_state = self.gru(gru_inputs, hidden.unsqueeze(0))
-        embedding = embedding.squeeze(0)
-        output = output.squeeze(0)
-        context_vector = context_vector
-        output = self.fc(
-            torch.cat((embedding, context_vector, output), dim=-1))
+        rnn_input = torch.cat((embedding, torch.unsqueeze(context_vector, dim=0)), dim=-1)
+        rnn_output, dec_state = self.rnn(rnn_input, hidden.unsqueeze(dim=0))
+        embedding = embedding.squeeze(dim=0)
+        rnn_output = rnn_output.squeeze(dim=0)
+        output = self.fc(torch.cat((embedding, context_vector, rnn_output), dim=-1))
 
         return output, dec_state.squeeze(0)
